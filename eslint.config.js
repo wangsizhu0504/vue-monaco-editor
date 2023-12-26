1b6851e436336ccddd5f645c5062b9fbe3539f44
Kriszu <39726513+wangsizhu0504@users.noreply.github.com>
2023-12-26 15:29:27 +0800

    chore: init

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import kriszu from '@kriszu/eslint-config'
+
+export default kriszu({
+  rules: {
+    'ts/prefer-for-of': 'off',
+    'no-restricted-globals': 'off',
+    'ts/consistent-type-assertions': 'off',
+    'jsdoc/no-multi-asterisks': 'off',
+  },
+})
